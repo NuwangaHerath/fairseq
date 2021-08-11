af6aa8a0b5b350c639cda8a53fcaa91b3c64cbf1
nuwanga <nuwangamihiruwan@gmail.com>
2021-08-12 01:47:40 +0530

    test 8

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -490,7 +490,7 @@ class Trainer(object):
                 for param in self.model.encoder.parameters():
                     # param = encoder_params[m]
                     if m == 11:
-                        logger.info(param)
+                        logger.info(type(param))
                     m = m + 1
                     logger.info("Encoder parameter replaced with loaded encoder parameter!")
 
