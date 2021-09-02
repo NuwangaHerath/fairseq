6a11887efa01ba733035b8cab606082aee9f4e08
nuwanga <nuwangamihiruwan@gmail.com>
2021-09-02 19:38:23 +0530

    changes for mbart transfer learning on UNIGPU

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -426,8 +426,7 @@ class Trainer(object):
         """
         # load pre-saved encoder
         encoder_model = self.model
-        #encoder_model.load_state_dict(torch.load('10_fairseq/si_en/checkpoints_5k/checkpoint480.pt'))
-        encoder_checkpoint = torch.load('10_fairseq/si_en/checkpoints_5k/checkpoint480.pt')
+        encoder_checkpoint = torch.load('/userdirs/halogen17/sien50k/models/checkpoints/checkpoint_111_90000.pt')
         encoder_model.load_state_dict(encoder_checkpoint['model'])
         encoder_params = []
         for param in encoder_model.encoder.parameters():
