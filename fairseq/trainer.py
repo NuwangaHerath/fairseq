8ff5ef36cc9e42b3dbf8e0e0d47e132f8242dd92
nuwanga <nuwangamihiruwan@gmail.com>
2021-09-05 22:57:59 +0530

    add encoder model checkpoint path

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -426,7 +426,7 @@ class Trainer(object):
         """
         # load pre-saved encoder
         encoder_model = self.model
-        encoder_checkpoint = torch.load('/userdirs/halogen17/sien50k/models/checkpoints/checkpoint_111_90000.pt')
+        encoder_checkpoint = torch.load('10_fairseq/si_en_cross/checkpoints/checkpoint490.pt')
         encoder_model.load_state_dict(encoder_checkpoint['model'])
         encoder_params = []
         for param in encoder_model.encoder.parameters():
